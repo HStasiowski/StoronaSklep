98016eb97927a60dbc2a45837012b0e375a0376d
Vitalii Morskyi <vitalii@morskyi.dev>
2023-02-08 02:39:08 +0000

    Switch to production

--- sklepinternetowy/SklepInternetowy/urls.py
+++ sklepinternetowy/SklepInternetowy/urls.py
@@ -22,8 +22,8 @@ urlpatterns = [
     path("main/", include("main.urls")),
     path("admin/", admin.site.urls),
 ]
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = "main.views.handler400_view"
 handler500 = "main.views.handler403_view"
